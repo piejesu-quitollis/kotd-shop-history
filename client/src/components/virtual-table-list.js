cab7468c917b3f25542369a4f6e5926371380145
piejesu <piejesu.quitollis@gmail.com>
2025-06-04 18:18:31 +0300

    fix: remove unused useRef

--- client/src/components/virtual-table-list.js
+++ client/src/components/virtual-table-list.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import TableCard from './table-card';
 
 function VirtualTableList() {
