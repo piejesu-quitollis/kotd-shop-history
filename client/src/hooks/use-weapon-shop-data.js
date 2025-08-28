f8b6afdf573c915724410648c59aef1fd9fa37bf
piejesu-quitollis <piejesu.quitollis@gmail.com>
2025-08-28 16:55:04 +0300

    Update use-weapon-shop-data.js
    eslint from mobile

--- client/src/hooks/use-weapon-shop-data.js
+++ client/src/hooks/use-weapon-shop-data.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useRef } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { getLatestDaily } from '../services/functions-client';
 
 export default function useWeaponShopData() {
